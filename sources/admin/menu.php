ac5e3b57826ce2d670308b15ab2ac673dff51ef1
julienbouche <julien.bouche@gmail.com>
2013-11-07 10:48:20 +0100

    ajout menu pages admin

---
+++ sources/admin/menu.php
@@ -0,0 +1,5 @@
+
+<div align="center">
+    <div class="MessagerieMenuItem"><a href="index.php" class="vBibLink" ><input value="Variables" type="button" /></a></div>
+    <div class="MessagerieMenuItem"><a href="manage_roles.php" class="vBibLink" ><input value="Groupes" type="button" /></a></div>
+</div>
